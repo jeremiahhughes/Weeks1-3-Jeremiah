9df738508c2c79b1017e97db99a4c2d5f1e76f78
Jeremiah Hughes <hughjere@shernet.sheridancollege.ca>
2026-01-14 10:21:04 -0500

    Did an AnimationCurve easing the lerp

--- Assets/Scripts/LinearINterpolation1.cs
+++ Assets/Scripts/LinearINterpolation1.cs
@@ -5,6 +5,7 @@ public class LinearINterpolation1 : MonoBehaviour
     public Transform start;
     public Transform end;
     public float t = 0;
+    public AnimationCurve curve;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,6 +20,10 @@ public class LinearINterpolation1 : MonoBehaviour
         {
             t = 0;
         }
-     transform.position = Vector2.Lerp(start.position, end.position, t);
+
+      //  transform.position = Vector2.Lerp(start.position, end.position, t;
+        transform.position = Vector2.Lerp(start.position, end.position, curve.Evaluate(t));
+
+      
     }
 }
