831f6b8a4002304a4b5f58dfbb30f585b53ab773
Jeremiah Hughes <hughjere@shernet.sheridancollege.ca>
2026-01-21 10:32:10 -0500

    Changing how the sprites work and how many you can have in the scene

--- Assets/Scripts/SpriteChanger.cs
+++ Assets/Scripts/SpriteChanger.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,39 +6,51 @@ public class SpriteChanger : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Color col;
+    public Sprite[] barrels = new Sprite[3];
+    public int randomNumber;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //PickARandomColor();
+        //PickARandomColor();   
+        PickARandomSprite();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if(Keyboard.current.anyKey.wasPressedThisFrame == true)
-        //{
-        //    //PickARandomColor();
-        //
-
-        // not this one!!!! spriteRenderer.sprite.bounds.Contains(mousePos) this is at 0, 0 
-        // use this one!!!!!! spriteRenderer.bounds.Contains(mousePos) its at the right position
-
-        //get mouse position 
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        // is it over the sprite
-        if(spriteRenderer.bounds.Contains(mousePos) == true)
+        if (Keyboard.current.anyKey.wasPressedThisFrame == true)
         {
-            //y use the col variable 
-            spriteRenderer.color = col;
-        }
-        else
-        {
-            spriteRenderer.color = Color.white;
+            //PickARandomColor();
+
+
+            // not this one!!!! spriteRenderer.sprite.bounds.Contains(mousePos) this is at 0, 0 
+            // use this one!!!!!! spriteRenderer.bounds.Contains(mousePos) its at the right position
+
+            //get mouse position 
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            // is it over the sprite
+            if (spriteRenderer.bounds.Contains(mousePos) == true)
+            {
+                //y use the col variable 
+                spriteRenderer.color = col;
+            }
+            else
+            {
+                spriteRenderer.color = Color.white;
+            }
         }
     }
-
-    void PickARandomColor() 
+    void PickARandomColor()
     {
         spriteRenderer.color = Random.ColorHSV();
+    }
+
+    void PickARandomSprite()
+    {
+        //get a random number between 0 and 2 
+        randomNumber = Random.Range(0, barrels.Length);
+
+        //use that to set the sprite
+        spriteRenderer.sprite = barrels[randomNumber];
     }
 }
