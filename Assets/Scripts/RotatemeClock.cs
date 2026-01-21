33437f5545fc6b7f2270cf8eebcd9104ca271753
Jeremiah Hughes <hughjere@shernet.sheridancollege.ca>
2026-01-21 11:31:21 -0500

    making the hour hand rotate coding gym

---
+++ Assets/Scripts/RotatemeClock.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class RotatemeClock : MonoBehaviour
+{
+    public float speed = -600;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newRotation = transform.eulerAngles;
+        newRotation.z += speed * Time.deltaTime;
+        transform.eulerAngles = newRotation;
+    }
+}
