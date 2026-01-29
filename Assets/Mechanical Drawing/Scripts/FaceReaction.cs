cf354f8ea3b45752c807598b0c287155ba360f6b
jeremiahhughes <148461474+jeremiahhughes@users.noreply.github.com>
2026-01-28 20:08:47 -0500

    Changing things in my faceReaction script that were out of scope for assignement 1

--- Assets/Mechanical Drawing/Scripts/FaceReaction.cs
+++ Assets/Mechanical Drawing/Scripts/FaceReaction.cs
@@ -2,11 +2,9 @@ using UnityEngine;
 
 public class FaceReaction : MonoBehaviour
 {
-    // Variables for changing the mouth of the face sprite from normal to happy
-    public GameObject normalFace;
-    public GameObject happyFace;
+    // Variables to change Scale of my face game object
     Vector3 normalScale = new Vector2(0.5f, 0.5f);
-    Vector3 happyScale = new Vector2(0.7f, 0.7f);
+    Vector3 biggerScale = new Vector2(0.7f, 0.7f);
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -20,18 +18,12 @@ public class FaceReaction : MonoBehaviour
     { 
         if (MusicNote.isHovering)
         {
-            // Swap to happy face
-            normalFace.SetActive(false);
-            happyFace.SetActive(true);
-            // Using Transform.localScale and Vector 3.Lerp to change the scale of the normal face back to the happy face
-            transform.localScale = Vector3.Lerp(transform.localScale, happyScale, Time.deltaTime * 10);
+            // Using Transform.localScale and Vector 3.Lerp to change the scale to make the face bigger in the bottom
+            transform.localScale = Vector3.Lerp(transform.localScale, biggerScale, Time.deltaTime * 10);
         }
         else
         {
-            // Swap to normal face
-            normalFace.SetActive(true);
-            happyFace.SetActive(false);
-            // Using Transform.localScale and Vector 3.Lerp to change the scale of the happy face back to the normal face
+            // Using Transform.localScale and Vector 3.Lerp to change the make the face go back to the normal scale in the bottom
             transform.localScale = Vector3.Lerp(transform.localScale, normalScale, Time.deltaTime * 10);
         }
 
